55e247769d45c30a2c75fa055aa8eef4e5dbd6cd
JG-e <zezhengh@163.com>
2024-09-16 20:58:16 -0400

    Refactor write_ppm function to support writing both P2 and P3 formats

--- src/write_ppm.cpp
+++ src/write_ppm.cpp
@@ -13,8 +13,26 @@ bool write_ppm(
   assert(
     (num_channels == 3 || num_channels ==1 ) &&
     ".ppm only supports RGB or grayscale images");
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  using namespace std;
+  unsigned char p2orp3 = (num_channels == 3) ? 0 : 1; // a flag showing if p2 or p3, 0 as p2, 1 as p3;
+  int len = width * height;
+  ofstream f; 
+  f.open(filename);
+  // Write header number according to the file format
+  if (p2orp3) {
+    f << "P3" << endl;
+  } 
+  else { 
+    f << "P2" << endl;
+  }
+  // width and height according to the protocol
+  f << width << " " << height << endl;
+  // set up max value
+  f << 255 << endl; 
+  for (int i = 0; i < len; i++) {
+      f << data[i] << " ";
+    if ((i * num_channels) % width == 0)
+      f << endl; // makes sure it is nicely printed for visibility
+  }
+  
 }
