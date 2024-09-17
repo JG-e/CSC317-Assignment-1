18227750213e4725fbbf602c8fa2e020a023192d
JG-e <zezhengh@163.com>
2024-09-17 17:33:16 -0400

    Refactor demosaic function to fix indexing bug and improve readability

--- src/demosaic.cpp
+++ src/demosaic.cpp
@@ -1,7 +1,15 @@
 #include "demosaic.h"
 std::vector<int> get_neighbours(int index, int width, int height);
-void increment_and_plus(int& num, int& val, std::vector<int> increment_vals);
-void demosaic(
+
+void increment_and_plus(
+  int& num, 
+  int& val, 
+  std::vector<int> neighbour_indices, 
+  std::vector<int> neighbours,
+  const std::vector<unsigned char> & bayer
+  );
+  
+  void demosaic(
   const std::vector<unsigned char> & bayer,
   const int & width,
   const int & height,
@@ -24,62 +32,84 @@ void demosaic(
   // For each pixel
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
-      int pixel = x + y * height;
-    // if I have the exact value, take it (Say I am on a green valued grayscale pixel)
-    if (y % 2 == 0) {
-      if (x % 2 == 0) { 
-        rgb_flag = 1;
-        g_val = bayer[pixel];
+      // Initialize these values for every pixel 
+      num_r = 0; 
+      num_g = 0;
+      num_b = 0; 
+      r_val = 0; 
+      g_val = 0; 
+      b_val = 0;
+      int curr_pixel_i = y * width + x;
+      // if I have the exact value, take it 
+      // (Say I am on a green valued grayscale pixel, I'll use the green value)
+      if (y % 2 == 0) {
+        // Even index row is GBGB...
+        if (x % 2 == 0) { 
+          // GBGB starts with G
+          rgb_flag = 1;
+          num_g++;
+          g_val = bayer[curr_pixel_i];
+        }
+        else {
+          // GBGB is then B
+          rgb_flag = 2;
+          num_b++;
+          b_val = bayer[curr_pixel_i];
+        }
       }
       else {
-        rgb_flag = 2;
-        b_val = bayer[pixel];
-      }
-    }
-    else {
-      if (x % 2 == 0) {
-        rgb_flag = 0;
-        r_val = bayer[pixel];
-      }
-      else {
-        rgb_flag = 1;
-        g_val = bayer[pixel];
-      }
-    } 
-    // Then take the average of the neighbours' R and B values (Take from R neighbours
-    // if calculating R, and vice versa).
-    std::vector<int> neighbours = get_neighbours(pixel, width, height);
+        // Odd index rows are RGRG
+        if (x % 2 == 0) {
+          // Starts with a R
+          rgb_flag = 0;
+          num_r++;
+          r_val = bayer[curr_pixel_i];
+        }
+        else {
+          // Then with a G
+          rgb_flag = 1;
+          num_g++;
+          g_val = bayer[curr_pixel_i];
+        }
+      } 
+      // Then take the average of the neighbours' R and B values (Take from R neighbours
+      // if calculating R, and vice versa).
+      std::vector<int> neighbours = get_neighbours(curr_pixel_i, width, height);
 
-    switch (rgb_flag) {
-      case 0: 
-        // Red case: Left, right, up, down are green, the 4 corners are blue
-        increment_and_plus(num_g, g_val, {3, 4, 1, 6});
-        increment_and_plus(num_b, b_val, {0, 2, 5, 7});
-        g_val /= num_g; 
-        b_val /= num_b;
-        break;
-      case 1: 
-        // Green case: odd row: up down are red. left right are blue. even row: 
-        // up down are blue. Left right are red. 
-        increment_and_plus(num_r, r_val, {1, 6});
-        increment_and_plus(num_b, b_val, {3, 4});
-        r_val /= num_r; 
-        b_val /= num_b;
-        break;
-      case 2:
-        // Blue case: up down left right are green, corners are red
-        increment_and_plus(num_g, g_val, {3, 4, 1, 6});
-        increment_and_plus(num_r, r_val, {0, 2, 5, 7});
-        break;
-        g_val /= num_g;
-        num_r /= r_val;
-    }
-
-    // Assign the recovered rgb values back to rgb
-    int rgb_pixel = pixel * 3;
-    rgb[rgb_pixel] = r_val;
-    rgb[rgb_pixel + 1] = g_val; 
-    rgb[rgb_pixel + 2] = b_val;
+      switch (rgb_flag) {
+        case 0: 
+          // Red case: Left, right, up, down are green, the 4 corners are blue
+          increment_and_plus(num_g, g_val, {3, 4, 1, 6}, neighbours, bayer);
+          increment_and_plus(num_b, b_val, {0, 2, 5, 7}, neighbours, bayer);
+          break;
+        case 1: 
+          // Green case: even index row: up down are red. left right are blue. 
+          if (y % 2 == 0) {
+            increment_and_plus(num_r, r_val, {1, 6}, neighbours, bayer);
+            increment_and_plus(num_b, b_val, {3, 4}, neighbours, bayer);
+            // increment_and_plus(num_g, g_val, {0, 2, 5, 7}, neighbours, bayer);
+          }
+          // Odd index row: up down are blue. Left right are red. The 4 corners are green.
+          else {
+            increment_and_plus(num_r, r_val, {3, 4}, neighbours, bayer);
+            increment_and_plus(num_b, b_val, {1, 6}, neighbours, bayer);
+            // increment_and_plus(num_g, g_val, {0, 2, 5, 7}, neighbours, bayer);
+          }
+          break;
+        case 2:
+          // Blue case: up down left right are green, corners are red
+          increment_and_plus(num_g, g_val, {3, 4, 1, 6}, neighbours, bayer);
+          increment_and_plus(num_r, r_val, {0, 2, 5, 7}, neighbours, bayer);
+          break;
+      }
+      r_val /= num_r; 
+      b_val /= num_b;
+      g_val /= num_g;
+      // Assign the recovered rgb values back to rgb
+      int rgb_pixel = curr_pixel_i * 3;
+      rgb[rgb_pixel] = num_r == 0 ? 0 : static_cast<unsigned char>(r_val);
+      rgb[rgb_pixel + 1] = num_g == 0 ? 0 : static_cast<unsigned char>(g_val); 
+      rgb[rgb_pixel + 2] = num_b == 0 ? 0 : static_cast<unsigned char>(b_val);
     }
   }
   ////////////////////////////////////////////////////////////////////////////
@@ -118,11 +148,19 @@ std::vector<int> get_neighbours(int index, int width, int height) {
     return neighbors;
 }
 
-void increment_and_plus(int& num, int& val, std::vector<int> increment_vals) { 
-  for (int v: increment_vals) {
-    if (v > -1) {
+void increment_and_plus(
+  int& num, 
+  int& val, 
+  std::vector<int> neighbour_indices, 
+  std::vector<int> neighbours,
+  const std::vector<unsigned char> & bayer
+  ) { 
+  for (int i: neighbour_indices) {
+      int bayer_i = neighbours[i];
+      if (bayer_i == -1)
+        continue;
+      int v = bayer[bayer_i];
       num++;
       val += v;
-    }
   }
 }
