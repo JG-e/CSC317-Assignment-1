e6a65f7979410d764417d6882ca8f37add1eeee8
JG-e <zezhengh@163.com>
2024-09-17 19:20:18 -0400

    Refactor hue_shift function to correctly shift the hue of RGB values

--- src/hue_shift.cpp
+++ src/hue_shift.cpp
@@ -2,6 +2,8 @@
 #include "hsv_to_rgb.h"
 #include "rgb_to_hsv.h"
 
+unsigned char cast_rgb(double val); 
+
 void hue_shift(
   const std::vector<unsigned char> & rgb,
   const int width,
@@ -11,6 +13,18 @@ void hue_shift(
 {
   shifted.resize(rgb.size());
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+  double h, s, v, r, g, b;
+  for (int i = 0; i < width * height * 3; i+=3) {
+    rgb_to_hsv(rgb[i], rgb[i+1], rgb[i+2], h, s, v);
+    h = abs(fmod(h + shift, 360));
+    hsv_to_rgb(h, s, v, r, g, b); 
+    shifted[i] = cast_rgb(r); 
+    shifted[i+1] = cast_rgb(g);
+    shifted[i+2] = cast_rgb(b);
+  }
   ////////////////////////////////////////////////////////////////////////////
+}
+
+unsigned char cast_rgb(double val) {
+  return (unsigned char) (val * 255);
 }
