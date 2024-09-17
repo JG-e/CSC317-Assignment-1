8a4f5db2a5066f43a6b5749127a5bbf9ff0a8f94
JG-e <zezhengh@163.com>
2024-09-17 10:38:59 -0400

    Refactor simulate_bayer_mosaic function to implement bayer mosaic algorithm

--- src/simulate_bayer_mosaic.cpp
+++ src/simulate_bayer_mosaic.cpp
@@ -8,6 +8,27 @@ void simulate_bayer_mosaic(
 {
   bayer.resize(width*height);
   ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
+  /*
+  This bayer mosaic turns a regular rgb to mosaic by taking the intensity of the 
+  interleaving order of the rgb values. The order is Green, Blue, Green, Blue ...
+  for the odd row, and Red, Green, Red, Green, ... for the even row
+  */
+
+  // Keep track of the row 
+  int curr_row = 1;
+  int len = width * height;
+  for (int i = 0; i < len; i++) {
+  // If row # is odd, then go with GBGB...
+    if (curr_row % 2 == 1) {
+      bayer[i] = i % 2 == 0 ? rgb[i * 3 + 1] : rgb[i * 3 + 2];
+    }
+  // If row # is even, then RGRG...
+    else {
+      bayer[i] = i % 2 == 0 ? rgb[i * 3] : rgb [i * 3 + 1];
+    }
+  // Increment row if i is multiple of width
+    if (i > 0 && (i % width) == 0)  
+      curr_row++;
+  }
   ////////////////////////////////////////////////////////////////////////////
 }
