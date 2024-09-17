983233365a3b85b7d79785a0a8430b1ac58c74e2
JG-e <zezhengh@163.com>
2024-09-17 15:43:50 -0400

    Refactor rotate function to fix pixel rotation bug

--- src/rotate.cpp
+++ src/rotate.cpp
@@ -14,23 +14,18 @@ void rotate(
   pixels starting from the bottom, row 2's pixels become column 2's 
   pixels starting from the bottom, etc.
   */
-  int len = width * height;
-  // Track the current pixel of input and rotated
-  int curr_row = 0;
   int curr_input; 
   int curr_rotate;
   // Put the RGB or greyscale value in the rotated 
-  for (int i = 0; i < len; i++) {
-    // Increment input and rotated pixel by number of pixels
-    curr_input = i * num_channels; 
-    curr_rotate = (height * (width - 1 - i) + curr_row) * num_channels;
-    for (int channel = 0; channel < num_channels; channel++) {
-      // Copy every pixels' channel value
-      rotated[curr_rotate + channel] = input[curr_input + channel];
-    }
-    // Once reaches the end of the row, update the row
-    if (i > 0 && (i % width) == 0) { 
-      curr_row++;
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      // Increment input and rotated pixel by number of pixels
+      curr_input = (i * width + j) * num_channels; 
+      curr_rotate = (height * (width - j - 1) + i) * num_channels;
+      for (int channel = 0; channel < num_channels; channel++) {
+        // Copy every pixels' channel value
+        rotated[curr_rotate + channel] = input[curr_input + channel];
+      }
     }
   }
   ////////////////////////////////////////////////////////////////////////////
